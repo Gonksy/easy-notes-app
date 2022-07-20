5037122a4500138fe04d549b5dc7202e0c281b6d
Ahmad Yosry <realbulletdragon@gmail.com>
2022-07-20 19:49:41 +0200

    added controllers, models, and routes

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
-require('dotenv').config()
+const mongoose = require('mongoose');
 
+require('dotenv').config()
+require('./app/routes/note.routes.js')(app)
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'easy-notes-app'
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConnectionStr, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database");    
+}).catch(err => {
+  console.log('Could not connect to the database. Exiting now...', err);
+  process.exit();
+});
+
 
 app.get('/', (req, res) => {
   res.json({'message': 'Welcome to EasyNotes.'})
