f5361ae442ff43648eaf0c86b62ee6aa8d6741b3
Ahmad Yosry <realbulletdragon@gmail.com>
2022-07-26 07:11:10 +0200

    removed landing page

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -3,13 +3,13 @@ module.exports = (app) => {
 
 
    // Landing page
-   app.get('/', notes.landingPage)
+  //  app.get('/', notes.landingPage)
 
    // Create a new Note
    app.post('/notes', notes.create);
 
    // Retrieve all Notes
-   app.get('/notes', notes.findAll);
+   app.get('/', notes.findAll);
 
    // Retrieve a single Note with noteId
    app.get('/notes/:noteId', notes.findOne);
